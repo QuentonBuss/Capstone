fe0cf5c1cd1097d77e322ac65e335b516201878b
QuentonBuss <45429051+QuentonBuss@users.noreply.github.com>
2018-12-09 23:51:03 -0500

    Add files via upload

---
+++ HomeworkTracker5000/HomeworkTracker5000/Assignments.cs
@@ -0,0 +1,8 @@
+﻿using System; namespace HomeworkTracker5000 {     public class Assignments      {         NMCCourses _courseName;         string _assignmentName;         DateTime _dueDate;         private double _score;         string assignmentName = null;
+
+        #region Properties         public double Score         {             get { return _score; }             set { _score = value; }         }          public NMCCourses CourseName         {             get { return _courseName; }             set { _courseName = value; }         }          public string AssignmentName         {             get { return _assignmentName; }             set { _assignmentName = value; }         }          public DateTime DueDate         {             get { return _dueDate; }             set { _dueDate = value; }         }          public Assignments()         {          }          public string AssignmentNameMethod()         {             return _assignmentName;         }          public Assignments(string assignmnetName)         {             _assignmentName = assignmentName;         }
+        #endregion 
+        //
+        //Enum of Courses
+        //
+        public enum NMCCourses         {             CIT110,             EET102,             HIT112,             MTH121         }     } }    
